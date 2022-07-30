f895ff62f554cb6d7045bcf5ea60a2f02bd1251e
OwlCodR <max.lyovkin@mail.ru>
2022-07-31 00:19:06 +0300

    :recycle: Refactored style files

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,42 +1,75 @@
 import 'dart:ui';
 
 class AppColors {
-  static final light = _AppColorsLight();
-  static final dark = _AppColorsDark();
+  static final light = AppColorsTheme(
+    backPrimary: const Color(0xFFF7F6F2),
+    backSecondary: const Color(0xFFFFFFFF),
+    backElevated: const Color(0xFFFFFFFF),
+    lightGray: const Color(0xFFD1D1D6),
+    gray: const Color(0xFF8E8E93),
+    white: const Color(0xFFFFFFFF),
+    blue: const Color(0xFF007AFF),
+    green: const Color(0xFF34C759),
+    red: const Color(0xFFFF3B30),
+    labelPrimary: const Color(0xFF000000),
+    labelSecondary: const Color(0x99000000),
+    labelTertiary: const Color(0x4D000000),
+    labelDisable: const Color(0x26000000),
+    separator: const Color(0x33000000),
+    overlay: const Color(0x0F000000),
+  );
+
+  static final dark = AppColorsTheme(
+    backPrimary: const Color(0xFF161618),
+    backSecondary: const Color(0xFF252528),
+    backElevated: const Color(0xFF3C3C3F),
+    lightGray: const Color(0xFF48484A),
+    gray: const Color(0xFF8E8E93),
+    white: const Color(0xFFFFFFFF),
+    blue: const Color(0xFF0A84FF),
+    green: const Color(0xFF32D74B),
+    red: const Color(0xFFFF453A),
+    labelPrimary: const Color(0xFFFFFFFF),
+    labelSecondary: const Color(0x99FFFFFF),
+    labelTertiary: const Color(0x66FFFFFF),
+    labelDisable: const Color(0x26FFFFFF),
+    separator: const Color(0x33FFFFFF),
+    overlay: const Color(0x52000000),
+  );
 }
 
-class _AppColorsLight {
-  final backPrimary = const Color(0xFFF7F6F2);
-  final backSecondary = const Color(0xFFFFFFFF);
-  final backElevated = const Color(0xFFFFFFFF);
-  final lightGray = const Color(0xFFD1D1D6);
-  final gray = const Color(0xFF8E8E93);
-  final white = const Color(0xFFFFFFFF);
-  final blue = const Color(0xFF007AFF);
-  final green = const Color(0xFF34C759);
-  final red = const Color(0xFFFF3B30);
-  final labelPrimary = const Color(0xFF000000);
-  final labelSecondary = const Color(0x99000000);
-  final labelTertiary = const Color(0x4D000000);
-  final labelDisable = const Color(0x26000000);
-  final separator = const Color(0x33000000);
-  final overlay = const Color(0x0F000000);
-}
+class AppColorsTheme {
+  Color backPrimary;
+  Color backSecondary;
+  Color backElevated;
+  Color lightGray;
+  Color gray;
+  Color white;
+  Color blue;
+  Color green;
+  Color red;
+  Color labelPrimary;
+  Color labelSecondary;
+  Color labelTertiary;
+  Color labelDisable;
+  Color separator;
+  Color overlay;
 
-class _AppColorsDark {
-  final backPrimary = const Color(0xFF161618);
-  final backSecondary = const Color(0xFF252528);
-  final backElevated = const Color(0xFF3C3C3F);
-  final lightGray = const Color(0xFF48484A);
-  final gray = const Color(0xFF8E8E93);
-  final white = const Color(0xFFFFFFFF);
-  final blue = const Color(0xFF0A84FF);
-  final green = const Color(0xFF32D74B);
-  final red = const Color(0xFFFF453A);
-  final labelPrimary = const Color(0xFFFFFFFF);
-  final labelSecondary = const Color(0x99FFFFFF);
-  final labelTertiary = const Color(0x66FFFFFF);
-  final labelDisable = const Color(0x26FFFFFF);
-  final separator = const Color(0x33FFFFFF);
-  final overlay = const Color(0x52000000);
+  AppColorsTheme({
+    required this.backPrimary,
+    required this.backSecondary,
+    required this.backElevated,
+    required this.lightGray,
+    required this.gray,
+    required this.white,
+    required this.blue,
+    required this.green,
+    required this.red,
+    required this.labelPrimary,
+    required this.labelSecondary,
+    required this.labelTertiary,
+    required this.labelDisable,
+    required this.separator,
+    required this.overlay,
+  });
 }
--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -2,30 +2,88 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
 class AppThemes {
-  static ThemeData light = _lightTheme();
-  static ThemeData dark = _darkTheme();
+  static ThemeData light = _AppTheme.getTheme(
+    parent: ThemeData.light(),
+    textTheme: _AppTextTheme.getTheme(colors: AppColors.light),
+    colors: AppColors.light,
+  );
 
-  static ThemeData _lightTheme() {
-    final colors = AppColors.light;
+  static ThemeData dark = _AppTheme.getTheme(
+    parent: ThemeData.dark(),
+    textTheme: _AppTextTheme.getTheme(colors: AppColors.dark),
+    colors: AppColors.dark,
+  );
+}
 
-    return ThemeData.light().copyWith(
+class _AppTheme {
+  static ThemeData getTheme({
+    required ThemeData parent,
+    required TextTheme textTheme,
+    required AppColorsTheme colors,
+  }) {
+    return parent.copyWith(
       primaryColor: colors.backPrimary,
       cardColor: colors.white,
       backgroundColor: colors.backPrimary,
       scaffoldBackgroundColor: colors.backPrimary,
       disabledColor: colors.labelDisable,
+      textTheme: textTheme,
     );
   }
+}
 
-  static ThemeData _darkTheme() {
-    final colors = AppColors.dark;
-
-    return ThemeData.dark().copyWith(
-      primaryColor: colors.backPrimary,
-      cardColor: colors.white,
-      backgroundColor: colors.backPrimary,
-      scaffoldBackgroundColor: colors.backPrimary,
-      disabledColor: colors.labelDisable,
+class _AppTextTheme {
+  static TextTheme getTheme({required AppColorsTheme colors}) {
+    return TextTheme(
+      headlineLarge: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 32.0,
+        height: 37.5 / 32.0,
+        fontWeight: FontWeight.w500,
+        color: colors.labelPrimary,
+      ),
+      headlineMedium: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 20.0,
+        height: 32.0 / 20.0,
+        fontWeight: FontWeight.w500,
+        color: colors.labelPrimary,
+      ),
+      titleMedium: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 16.0,
+        height: 20.0 / 16.0,
+        fontWeight: FontWeight.w400,
+        color: colors.labelTertiary,
+      ),
+      bodyLarge: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 16.0,
+        height: 20.0 / 16.0,
+        fontWeight: FontWeight.w400,
+        color: colors.labelPrimary,
+      ),
+      bodyMedium: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 14.0,
+        height: 20.0 / 14.0,
+        fontWeight: FontWeight.w400,
+        color: colors.labelTertiary,
+      ),
+      labelLarge: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 16.0,
+        height: 18.0 / 16.0,
+        fontWeight: FontWeight.w400,
+        color: colors.labelTertiary,
+      ),
+      labelMedium: TextStyle(
+        fontFamily: 'Roboto',
+        fontSize: 14.0,
+        height: 16.41 / 14.0,
+        fontWeight: FontWeight.w400,
+        color: colors.labelTertiary,
+      ),
     );
   }
 }
