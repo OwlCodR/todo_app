eec3015efeb743efbfe82931de888a8ac1944544
OwlCodR <max.lyovkin@mail.ru>
2022-08-27 09:34:50 +0300

    :recycle: Added ImportanceLocaleArgs, moved models to data folder, removed useless jsonEncode in dio requests

--- lib/datasources/tasks_remote_datasource.dart
+++ lib/datasources/tasks_remote_datasource.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
 import 'package:dio_logging_interceptor/dio_logging_interceptor.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -7,10 +5,10 @@ import 'package:todo_app/constants/api_paths.dart';
 import 'package:todo_app/models/domain/task_model.dart';
 
 import '../interceptors/token_interceptor.dart';
+import '../models/data/remote/task_request.dart';
 import '../models/data/remote/task_response.dart';
+import '../models/data/remote/tasks_request.dart';
 import '../models/data/remote/tasks_response.dart';
-import '../models/task_request.dart';
-import '../models/tasks_request.dart';
 import '../utils/json_pretty_print.dart';
 import '../utils/logger.dart';
 
@@ -30,7 +28,7 @@ class TasksRemoteDatasource {
     log.d('[$runtimeType] createTask(${prettyString(newTask)})');
     return _dio.post(
       ApiPaths.list,
-      data: jsonEncode(TaskRequest(task: newTask).toJson()),
+      data: TaskRequest(task: newTask).toJson(),
       options: Options(
         headers: {
           lastKnownRevisionHeader: lastKnownRevision,
@@ -71,7 +69,7 @@ class TasksRemoteDatasource {
     log.d('[$runtimeType] updateTask(${prettyString(newTask)})');
     return _dio.put(
       '${ApiPaths.list}/${newTask.id}',
-      data: jsonEncode(TaskRequest(task: newTask).toJson()),
+      data: TaskRequest(task: newTask).toJson(),
       options: Options(
         headers: {
           lastKnownRevisionHeader: lastKnownRevision,
@@ -87,7 +85,7 @@ class TasksRemoteDatasource {
     log.d('[$runtimeType] updateList(${prettyString(tasks)})');
     final response = await _dio.patch(
       ApiPaths.list,
-      data: jsonEncode(TasksRequest(tasks: tasks).toJson()),
+      data: TasksRequest(tasks: tasks).toJson(),
       options: Options(
         headers: {
           lastKnownRevisionHeader: lastKnownRevision,
